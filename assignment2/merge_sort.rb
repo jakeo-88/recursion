ce07ed01d758a957c7017d985b44d63d46dc4983
Jake <jakegreen2016@gmail.com>
2020-11-25 15:01:58 -0900

    create method that divides array into two new arrays

--- assignment2/merge_sort.rb
+++ assignment2/merge_sort.rb
@@ -1,5 +1,10 @@
 # break the array into individual segments
-
+def break_array(ary)
+  return true if ary.length == 1
+        
+  Array.new(ary[0, (ary.length/2) ] )
+  Array.new(ary[-(ary.length/2), (ary.length/2) ] )
+end
 # sort the array
 
 # sort in pairs of 2
